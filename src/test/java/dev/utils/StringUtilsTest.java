ded8462897c4065fe45a087547e25b5dc2e0b323
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2019-11-27 17:36:25 +0100

    v1.0

---
+++ src/test/java/dev/utils/StringUtilsTest.java
@@ -0,0 +1,37 @@
+package dev.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+	@Test
+	public void testLevenshteinDistance1() {
+		int resultat = StringUtils.levenshteinDistance("lhs", "rhs");
+		assertEquals(1, resultat);
+	}
+
+	@Test
+	public void testLevenshteinDistance2() {
+		int resultat = StringUtils.levenshteinDistance("los", "rhs");
+		assertEquals(2, resultat);
+	}
+
+	@Test
+	public void testLevenshteinDistance3() {
+		int resultat = StringUtils.levenshteinDistance("lod", "rhs");
+		assertEquals(3, resultat);
+	}
+
+	@Test
+	public void testLevenshteinDistanceNull() {
+		int resultat =StringUtils.levenshteinDistance(null, "rhs");
+		assertEquals(0,resultat);
+	}
+	@Test
+	public void testLevenshteinDistanceNull1() {
+		int resultat =StringUtils.levenshteinDistance("lhs", null);
+		assertEquals(0,resultat);
+	}
+}
